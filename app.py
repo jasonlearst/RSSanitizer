930b754d68124e99977fee6c9ab55abf3b942513
Jason Learst <jason.learst@gmail.com>
2024-10-28 01:21:12 +0000

    removed unused stuff

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ from lxml import etree
 from flask import Flask, render_template, Response, make_response
 
 app = Flask(__name__)
-XKCD_RSS_URL = "https://podcast.wandwmusic.nl/podcast.php"
-XKCD_ATOM_URL = "https://www.xkcd.com/atom.xml"
+WW_RSS_URL = "https://podcast.wandwmusic.nl/podcast.php"
 
-XKCD_REQ_UA = 'xkcdmobile-feed-proxy jeremie@miserez.org'
-XKCD_REQ_TIMEOUT=2.0
+FEED_REQ_UA = 'xkcdmobile-feed-proxy jeremie@miserez.org'
+FEED_REQ_TIMEOUT=2.0
 
 COPY_HEADERS = [
 'Last-Modified', 
@@ -23,19 +22,12 @@ COPY_HEADERS = [
 'If-Unmodified-Since',
 ]
 
-REPLACEMENTS = {
-u"http://xkcd.com": u"http://m.xkcd.com",
-u"https://xkcd.com": u"https://m.xkcd.com",
-u"http://www.xkcd.com": u"http://m.xkcd.com",
-u"https://www.xkcd.com": u"https://m.xkcd.com",
-}
-
 @app.errorhandler(Exception)
 def all_exception_handler(error):
     flask.abort(Exception)
 
 @app.errorhandler(404)
-def all_exception_handler(error):
+def not_available_handler(error):
     return render_template('index.html'), 404
 
 @app.route("/", methods=['GET'])
@@ -44,23 +36,19 @@ def index():
 
 @app.route("/rss.xml", methods=['GET'])
 def rss():
-    return proxy_xkcd(XKCD_RSS_URL)
+    return proxy_rss(WW_RSS_URL)
 
-@app.route("/atom.xml", methods=['GET'])
-def atom():
-    return proxy_xkcd(XKCD_ATOM_URL)
-
-def proxy_xkcd(feed_url):
+def proxy_rss(feed_url):
     try:
         req_headers = {}
         for h,v in flask.request.headers:
             if h in COPY_HEADERS:    
                 req_headers[h] = v
         req_headers['Accept'] = '*/*' # needed, otherwise xkcd doesnt send etag header!?!
-        req_headers['User-Agent'] = XKCD_REQ_UA
+        req_headers['User-Agent'] = FEED_REQ_UA
 
         req = requests.Request('GET', feed_url, headers=req_headers).prepare()
-        r = requests.Session().send(req, verify=False, timeout=XKCD_REQ_TIMEOUT)
+        r = requests.Session().send(req, verify=False, timeout=FEED_REQ_TIMEOUT)
         return get_modified_response(r)
     except Exception as e:
         print(e)
@@ -68,7 +56,7 @@ def proxy_xkcd(feed_url):
 
 def get_modified_response(r):
     new_content = r.content
-    my_parser = etree.XMLParser(recover=True)
+    my_parser = etree.XMLParser(recover=True) # from SO: https://stackoverflow.com/a/59645304
     xml = etree.fromstring(new_content, parser=my_parser)
     cleaned_xml_string = etree.tostring(xml)
 
